63c042fdb791fa395408a4f3a7fa5435b121024a
UGYW <uma.w@outlook.com>
2019-11-13 14:05:17 -0800

    Added mock data and Gantt chart example.

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -3,7 +3,8 @@ package ui;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.Date;
-import javax.swing.JFrame;
+import javax.swing.*;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -11,79 +12,94 @@ import org.jfree.data.category.IntervalCategoryDataset;
 import org.jfree.data.gantt.Task;
 import org.jfree.data.gantt.TaskSeries;
 import org.jfree.data.gantt.TaskSeriesCollection;
+import org.jfree.data.time.SimpleTimePeriod;
+import org.jfree.data.time.TimePeriod;
 
 public class MainFrame extends JFrame {
 
-    private static final int MAIN_GUI_WIDTH = 800;
+    private static final int MAIN_GUI_WIDTH = 1200;
     private static final int MAIN_GUI_HEIGHT = 800;
 
     public MainFrame(String title) {
         super(title);
         // Create dataset
-        IntervalCategoryDataset dataset = getCategoryDataset();
+        IntervalCategoryDataset dataset = getMockDataset();
 
         // Create chart
         JFreeChart chart = ChartFactory.createGanttChart(
-                "Gantt Chart Example", // Chart title
-                "Software Development Phases", // X-Axis Label
+                "Multithread Visualization", // Chart title
+                "Threads", // X-Axis Label
                 "Timeline", // Y-Axis Label
                 dataset, true, false, false);
 
         ChartPanel panel = new ChartPanel(chart);
         setContentPane(panel);
+
+        setSize(MAIN_GUI_WIDTH, MAIN_GUI_HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
-    private IntervalCategoryDataset getCategoryDataset() {
+    private IntervalCategoryDataset getMockDataset() {
 
-        TaskSeries series1 = new TaskSeries("Estimated Date"); series1.add(new Task("Requirement",
+        TaskSeries series1 = new TaskSeries("Deadlocked");
+
+        series1.add(new Task("Thread 1",
                 Date.from(LocalDate.of(2017,7,3).atStartOfDay().toInstant(ZoneOffset.UTC)),
                 Date.from(LocalDate.of(2017, 7,7).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
 
-        series1.add(new Task("Design",Date.from(LocalDate.of(2017, 7,10).atStartOfDay().toInstant(ZoneOffset.UTC)),
+        series1.add(new Task("Thread 2",
+                Date.from(LocalDate.of(2017, 7,10).atStartOfDay().toInstant(ZoneOffset.UTC)),
                 Date.from(LocalDate.of(2017, 7, 14).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
 
-        series1.add(new Task("Coding",Date.from(LocalDate.of(2017, 7,17).atStartOfDay().toInstant(ZoneOffset.UTC)),
+        series1.add(new Task("Thread 3",
+                Date.from(LocalDate.of(2017, 7,17).atStartOfDay().toInstant(ZoneOffset.UTC)),
                 Date.from(LocalDate.of(2017, 7, 21).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
 
-        series1.add(new Task("Testing", Date.from(LocalDate.of(2017, 7,24).atStartOfDay().toInstant(ZoneOffset.UTC)),
-                Date.from(LocalDate.of(2017, 7, 28).atStartOfDay().toInstant(ZoneOffset.UTC))
-        ));
 
-        series1.add(new Task("Deployment", Date.from(LocalDate.of(2017, 07,31).atStartOfDay().toInstant(ZoneOffset.UTC)),
-                Date.from(LocalDate.of(2017, 8, 4).atStartOfDay().toInstant(ZoneOffset.UTC))
-        ));
+        TaskSeries series2 = new TaskSeries("Locked");
 
-
-        TaskSeries series2 = new TaskSeries("Actual Date");
-        series2.add(new Task("Requirement",Date.from(LocalDate.of(2017, 7,3).atStartOfDay().toInstant(ZoneOffset.UTC)),
+        series2.add(new Task("Thread 1",
+                Date.from(LocalDate.of(2017, 7,3).atStartOfDay().toInstant(ZoneOffset.UTC)),
                 Date.from(LocalDate.of(2017, 7, 05).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
 
-        series2.add(new Task("Design",
+        series2.add(new Task("Thread 2",
                 Date.from(LocalDate.of(2017, 7, 6).atStartOfDay().toInstant(ZoneOffset.UTC)),
                 Date.from(LocalDate.of(2017, 7, 17).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
 
-        series2.add(new Task("Coding",
+        series2.add(new Task("Thread 3",
                 Date.from(LocalDate.of(2017, 7, 18).atStartOfDay().toInstant(ZoneOffset.UTC)),
                 Date.from(LocalDate.of(2017, 7, 27).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
 
-        series2.add(new Task("Testing",
-                Date.from(LocalDate.of(2017, 7, 28).atStartOfDay().toInstant(ZoneOffset.UTC)),
-                Date.from(LocalDate.of(2017, 8, 1).atStartOfDay().toInstant(ZoneOffset.UTC))
+        TaskSeries series3 = new TaskSeries("Running");
+
+        series3.add(new Task("Thread 1",
+                Date.from(LocalDate.of(2017, 7,2).atStartOfDay().toInstant(ZoneOffset.UTC)),
+                Date.from(LocalDate.of(2017, 7, 4).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
 
-        series2.add(new Task("Deployment",
-                Date.from(LocalDate.of(2017, 8, 2).atStartOfDay().toInstant(ZoneOffset.UTC)),
-                Date.from(LocalDate.of(2017, 8, 4).atStartOfDay().toInstant(ZoneOffset.UTC))
+        series3.add(new Task("Thread 2",
+                Date.from(LocalDate.of(2017, 7, 9).atStartOfDay().toInstant(ZoneOffset.UTC)),
+                Date.from(LocalDate.of(2017, 7, 12).atStartOfDay().toInstant(ZoneOffset.UTC))
         ));
+
+        series3.add(new Task("Thread 3",
+                Date.from(LocalDate.of(2017, 7, 16).atStartOfDay().toInstant(ZoneOffset.UTC)),
+                Date.from(LocalDate.of(2017, 7, 17).atStartOfDay().toInstant(ZoneOffset.UTC))
+        ));
+
 
         TaskSeriesCollection dataset = new TaskSeriesCollection();
-        dataset.add(series1);dataset.add(series2);
+        dataset.add(series1);
+        dataset.add(series2);
+        dataset.add(series3);
         return dataset;
     }
 
